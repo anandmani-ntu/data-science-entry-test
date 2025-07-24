c5d5f6a70dcd5a86270c702f2e87501c1012c122
anandmani-ntu <anandmani@yahoo.com>
2025-07-24 15:05:43 +0800

    Add files via upload

---
+++ q6.py
@@ -0,0 +1,18 @@
+def find_first_negative(lst):
+    """
+    Task 1
+    - Create a function that finds the first negative number in a list (lst).
+    - Return the first negative number if found, otherwise return "No negatives".
+    - Use a while loop to implement this.
+    """
+    index = 0
+    while index < len(lst):
+        if lst[index] < 0:
+            return lst[index]
+        index += 1
+    return "No negatives"
+
+# Task 2
+# Invoke the function "find_first_negative" using the following scenario:
+result1 = find_first_negative([3, 5, -1, 7, -2, 8])  # This will return -1
+result2 = find_first_negative([2, 10, 7, 0])         # This will return "No negatives"
